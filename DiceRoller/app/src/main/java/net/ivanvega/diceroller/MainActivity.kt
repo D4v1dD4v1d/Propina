976c61eef5f3301233ff11de5ac6b3ca93623a14
D4v1dD4v1d <david.pz.zv@gmail.com>
2023-09-13 18:00:51 -0600

    Dice Roller

---
+++ DiceRoller/app/src/main/java/net/ivanvega/diceroller/MainActivity.kt
@@ -0,0 +1,107 @@
+package net.ivanvega.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import net.ivanvega.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme {
+                // A surface container using the 'background' color from the theme
+                DiceRollerApp()
+
+            }
+        }
+    }
+}
+
+private fun getRandomDiceImage() : Int {
+    return (1..6).random();
+}
+private fun getDiceImageResource(result:Int):Int{
+    return when(result){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+}
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
+    var result by remember {
+        mutableStateOf(1)
+    }
+    var result2 by remember {
+        mutableStateOf(1)
+    }
+
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = getDiceImageResource(result)),
+            contentDescription = stringResource(
+            R.string.imagen_de_dado
+            )
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(painter = painterResource(id = getDiceImageResource(result2)),
+            contentDescription = stringResource(
+                R.string.imagen_de_dado
+            )
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            result = getRandomDiceImage()
+            result2= getRandomDiceImage()
+        }
+        ) {
+            Text(stringResource(R.string.roll))
+        }
+    }
+
+
+}
+
+@Preview
+@Composable
+fun DiceRollerApp(){
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+
+    )
+
+
+}
